7bce84a1995d6e058950ebf00b472189b4b0b5ab
eddy madu <eddymadu90@gmail.com>
2023-06-25 22:00:14 +0100

    feat:app restructure

---
+++ src/core/router/router.ts
@@ -0,0 +1,59 @@
+
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+// nav components
+const HeaderFile=()=>import("../../components/HeaderFile.vue")
+const Homepage=()=>import( "../../components/Homepage.vue");
+const Servicespage=()=>import( "../../components/ProgramPage.vue");
+const aboutpage=()=>import( "../../components/CustomeCoach.vue");
+const contactpage=()=>import( "../../components/ContactPage.vue");
+const buttons=()=>import("../../components/Buttons.vue");
+const BreadCrumb=()=>import("../../components/BreadCrumb.vue");
+const FooterFile=()=>import("../../components/FooterFile.vue");
+
+
+const routes: Array<RouteRecordRaw> = [
+  { path: "/", redirect: "/home" },
+  { path: "/home", name: "page.home", components: {default:Homepage, NavBar: HeaderFile,FooterPage: FooterFile } },
+  { path: "/custome", name: "Custome Coaching", components: {default:aboutpage, NavBar: HeaderFile,banner: BreadCrumb,FooterPage: FooterFile } },
+  { path: "/services",name:"Programs", components: { default: Servicespage, NavBar: HeaderFile, banner: BreadCrumb, FooterPage: FooterFile, } },
+  { path: "/contact",name:"Contact Us", components: { default: contactpage,NavBar: HeaderFile, banner: BreadCrumb, FooterPage: FooterFile, } },
+//   { path: "/signup", components: { default: NewAccount, banner: banner, FooterPage: FooterPage } },
+//   { path: "/register", components: { default: RegisterPage, banner: banner, FooterPage: FooterPage, } },
+
+  // { path: "/secure/transfer", components: { dashheader: dashheader, sideBar:sideBar,default:transfer,baldash:dashboard  }},
+  {
+    path: "/:catchAll(.*)",
+    components: { default: HeaderFile, FooterPage: FooterFile, NavBar: HeaderFile } ,
+  },
+
+];
+
+const history = createWebHistory();
+const router = createRouter({
+  history,
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+  linkActiveClass:'activepage',
+});
+
+// router.beforeEach((route) => {
+//   if (route.meta && route.meta.requireAuth == true) {
+//     const isAuth = authenticate();
+//     if (isAuth == true) {
+//       return;
+//     } else {
+//       return (route.path = "/auth/login");
+//     }
+//   } else {
+//     return;
+//   }
+// });
+
+export default router;
